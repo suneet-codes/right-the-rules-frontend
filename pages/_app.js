2c4dd7c8f447eb573c15d8d2ed32d0c93c30848a
Suneet Misra <hello@suneet.codes>
2020-12-17 20:56:16 +0000

    Added linting packages and configuration with dot-files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import PropTypes from "prop-types"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.element,
+  pageProps: PropTypes.object,
 }
 
 export default MyApp
