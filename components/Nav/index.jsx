9ecde01408989ce9b281d12647ffda4f77a57504
Suneet Misra <hello@suneet.codes>
2020-12-19 22:42:06 +0000

    Added individual pages each nav-link and subsequent routing

--- components/Nav/index.jsx
+++ components/Nav/index.jsx
@@ -1,14 +1,35 @@
+import Link from "next/link"
 import { NavWrapper, NavLinks } from "./Nav.styles"
 
 const Nav = () => {
   return (
     <NavWrapper>
-      <div className="logo">Right the Rules</div>
+      <div className="logo">
+        <Link href="/">
+          <a>Right the Rules</a>
+        </Link>
+      </div>
       <NavLinks className="navLinks">
-        <li>our mission</li>
-        <li>about us</li>
-        <li>blog</li>
-        <li>contact</li>
+        <li>
+          <Link href="/our-mission">
+            <a>our mission</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about-us">
+            <a>about us</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/articles">
+            <a>articles</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>contact</a>
+          </Link>
+        </li>
       </NavLinks>
       <div className="hamburger">HBG</div>
     </NavWrapper>
