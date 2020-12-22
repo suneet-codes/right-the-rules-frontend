4b1ad4e96ea65422bbb642a219b38c403ef80c76
Suneet Misra <hello@suneet.codes>
2020-12-22 08:08:36 +0000

    Added typography elements

--- elements/Typography.js
+++ elements/Typography.js
@@ -8,6 +8,58 @@ export const H5 = styled.h5`
   font-weight: ${(props) => (props.bold ? 700 : 400)};
   margin: 0;
 `
+export const H4 = styled.h4`
+  font-family: ${(props) => props.theme.fonts.main};
+  font-style: normal;
+  font-size: 1.5625rem;
+  line-height: 1.875rem;
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+  margin: 0;
+`
+
+export const H3 = styled.h3`
+  font-family: ${(props) => props.theme.fonts.main};
+  font-style: normal;
+  font-size: 1.953rem;
+  line-height: 2.3125rem;
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+  margin: 0;
+`
+
+export const H2 = styled.h2`
+  font-family: ${(props) => props.theme.fonts.main};
+  font-style: normal;
+  font-size: 2.4375rem;
+  line-height: 2.9375rem;
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+  margin: 0;
+`
+
+export const H1 = styled.h1`
+  font-family: ${(props) => props.theme.fonts.main};
+  font-style: normal;
+  font-size: 3.0625rem;
+  line-height: 3.6875rem;
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+  margin: 0;
+`
+
+export const P = styled.p`
+  font-family: ${(props) => props.theme.fonts.main};
+  font-style: normal;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+  margin: 0;
+`
+export const Label = styled.p`
+  font-family: ${(props) => props.theme.fonts.main};
+  font-style: normal;
+  font-size: 0.75rem;
+  line-height: 0.875rem;
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+  margin: 0;
+`
 
 export const UL = styled.ul`
   margin: 0;
