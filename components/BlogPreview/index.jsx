f5cdcc8aec6fdba7bbdc151f6806f0f434322448
Suneet Misra <hello@suneet.codes>
2020-12-27 14:46:41 +0000

    basic layout added for BlogPreview homepage section

--- components/BlogPreview/index.jsx
+++ components/BlogPreview/index.jsx
@@ -1,21 +1,38 @@
 import PropTypes from "prop-types"
+import { useColorMode } from "../../hooks/useColorMode"
 import { BlogPreviewWrapper } from "./BlogPreview.styles"
 import { BlogPostCard } from "../../components"
 
-const BlogPreview = ({ articles }) => {
+const BlogPreview = ({ articles, colorType }) => {
+  const colorMode = useColorMode()
   return (
-    <BlogPreviewWrapper>
-      {articles.map((article) => {
-        return (
-          <BlogPostCard key={`${article.id}-${article.title}`} {...article} />
-        )
-      })}
+    <BlogPreviewWrapper
+      as="section"
+      colorType={colorType}
+      colorMode={colorMode.value}
+    >
+      <div className="container">
+        {articles.map((article) => {
+          return (
+            <BlogPostCard
+              key={`${article.id}-${article.title}`}
+              {...article}
+              className="card"
+            />
+          )
+        })}
+      </div>
     </BlogPreviewWrapper>
   )
 }
 
 BlogPreview.propTypes = {
   articles: PropTypes.array.isRequired,
+  colorType: PropTypes.string,
+}
+
+BlogPreview.defaultProps = {
+  colorType: "primary",
 }
 
 export default BlogPreview
