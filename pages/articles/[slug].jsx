89e03310e59b446fa5a7d23655d6c0278bc46e6b
Suneet Misra <hello@suneet.codes>
2020-12-28 22:50:06 +0000

    Moved article and articles wrappers into component

--- pages/articles/[slug].jsx
+++ pages/articles/[slug].jsx
@@ -1,11 +1,91 @@
 /** Dynamic page for rendering individual articles at the `/article` path */
+import styled from "@emotion/styled"
 import PropTypes from "prop-types"
 import ReactMarkdown from "react-markdown"
 import { fetchAPI } from "../../lib/api"
 import { useColorMode } from "../../hooks/useColorMode"
 import { getStrapiMedia } from "../../lib/media"
-import { Image, Seo, ArticleWrapper } from "../../components"
-import { H1, Label, Button } from "../../elements"
+import { colors } from "../../styles"
+import { Image, Seo } from "../../components"
+import { H1, Label, Button, Grid } from "../../elements"
+
+const ArticleWrapper = styled(Grid)`
+  background-color: ${(props) => props.theme[props.colorMode].colors.primary};
+  color: ${(props) => props.theme[props.colorMode].colors.textPrimary};
+  min-height: calc(100vh - 16rem);
+
+  .container {
+    grid-column: 2 / span 8;
+    margin-bottom: 3rem;
+    h1 {
+      text-align: center;
+      padding-top: 3rem;
+    }
+
+    p {
+      line-height: 2rem;
+    }
+    .content {
+      padding-top: 2.5rem;
+    }
+
+    .content p {
+      margin-bottom: 2rem;
+    }
+
+    .label {
+      padding-top: 2.5rem;
+      text-align: center;
+    }
+
+    .content a {
+      font-weight: 700;
+      color: ${colors.pink3};
+      &:hover {
+        color: ${colors.dark1};
+      }
+    }
+
+    .buttonContainer {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      a {
+        margin: 0rem 0.25rem;
+        width: 120px;
+        text-align: center;
+        p {
+          line-height: 1.5rem;
+        }
+      }
+    }
+  }
+
+  @media ${(props) => props.theme.breakpoints.tablet} {
+    .container {
+      grid-column: 3 / span 8;
+
+      /* h1 {
+        padding-top: 3rem;
+      } */
+
+      /* .label {
+        padding-top: 2.5rem;
+        text-align: center;
+      } */
+      /* .content {
+        padding-top: 2.5rem;
+      } */
+    }
+  }
+
+  @media ${(props) => props.theme.breakpoints.desktop} {
+    .container {
+      grid-column: 3 / span 10;
+    }
+  }
+`
 
 const Article = ({
   article,
--- pages/articles/index.jsx
+++ pages/articles/index.jsx
@@ -1,12 +1,55 @@
 import PropTypes from "prop-types"
+import styled from "@emotion/styled"
 import { useColorMode } from "../../hooks/useColorMode"
-import {
-  Articles,
-  Seo,
-  BlogPostCard,
-  ArticlesPageWrapper,
-} from "../../components"
+import { Articles, Seo, BlogPostCard } from "../../components"
+import { Grid } from "../../elements"
 import { fetchAPI } from "../../lib"
+
+const ArticlesPageWrapper = styled(Grid)`
+  background-color: ${(props) => props.theme[props.colorMode].colors.primary};
+  color: ${(props) => props.theme[props.colorMode].colors.textPrimary};
+  min-height: calc(100vh - 16rem);
+
+  .container {
+    grid-column: 1 / span 10;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: fit-content;
+
+    .card {
+      height: 300px;
+      justify-content: center;
+      .title {
+        max-width: 300px;
+      }
+    }
+  }
+
+  @media ${(props) => props.theme.breakpoints.tablet} {
+    .container {
+      grid-column: 1 / span 12;
+    }
+  }
+
+  @media ${(props) => props.theme.breakpoints.desktop} {
+    .container {
+      padding-top: 4rem;
+      grid-column: 2 / span 12;
+      flex-direction: row;
+      flex-wrap: wrap;
+      .card {
+        justify-content: space-between;
+        /* flex-basis: auto; */
+        min-height: 350px;
+        width: 33%;
+        background-color: ${(props) =>
+          props.theme[props.colorMode].colors.primary};
+        padding-bottom: 2rem;
+      }
+    }
+  }
+`
 
 const ArticlesPage = ({ articles, categories }) => {
   const colorMode = useColorMode()
